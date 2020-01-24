09333b333437b45e5b0e7dcf1029d2eb13ed60e2
Drauku Xan <drauku@gmail.com>
2020-01-24 13:38:59 -0600

    variables list update

---
+++ configs/docker_configs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "***** Creating Docker 'config' variables *****"
+# define config variables
+echo "techtinker.net" | docker config create var_domain -
+echo "cloudflare" | docker config create var_certresolver -
+echo "America" | docker config create var_tz_region -
+echo "Chicago" | docker config create var_tz_city -
+echo "1000" | docker config create var_usr -
+echo "100" | docker config create var_grp -
+
+#echo "" | docker config create var_ -
